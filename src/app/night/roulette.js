95dd9b9b8d7f1029d595627fcbffd60cea0a64a0
Other Aytay <OtherAytay@gmail.com>
2023-09-20 23:51:55 -0400

    Roll handling

--- src/app/night/roulette.js
+++ src/app/night/roulette.js
@@ -1,20 +1,20 @@
-var difficulty = "medium"
-const consoleOutput = true
+const default_difficulty = "medium"
+const consoleOutput = false
 
-function rollAll() {
-    rollButtplug()
-    rollUnderwear()
-    rollOutfit()
-    rollBDSM()
-    rollWrists()
-    rollAnkles()
+function rollAll(difficulty=default_difficulty) {
+    rollButtplug(difficulty)
+    rollUnderwear(difficulty)
+    rollOutfit(difficulty)
+    rollBDSM(difficulty)
+    rollWrists(difficulty)
+    rollAnkles(difficulty)
 }
 
-function rollButtplug() {
-    rollUse = r10()
-    result = [{rollType: "Use", roll: rollUse, decision: findDecision(buttplug[difficulty]["use"], rollUse)["use"]}]
+function rollButtplug(difficulty=default_difficulty) {
+    var rollUse = r10()
+    var result = [{rollType: "Use", roll: rollUse, decision: findDecision(buttplug[difficulty]["use"], rollUse)["use"]}]
     if (result[0].decision) {
-        rollSize = r10()
+        var rollSize = r10()
         result.push({rollType: "Size", roll: rollSize, decision: findDecision(buttplug[difficulty]["size"], rollSize)["size"]})
     }
 
@@ -22,44 +22,44 @@ function rollButtplug() {
     return result
 }
 
-function rollUnderwear() {
-    rollUpper = r10()
-    result = [{rollType: "Upper", roll: rollUpper, decision: findDecision(underwear[difficulty]["upper"], rollUpper)["upper"]}]
+function rollUnderwear(difficulty=default_difficulty) {
+    var rollUpper = r10()
+    var result = [{rollType: "Upper", roll: rollUpper, decision: findDecision(underwear[difficulty]["upper"], rollUpper)["upper"]}]
     
-    rollLower = r10()
+    var rollLower = r10()
     result.push({rollType: "Lower", roll: rollLower, decision: findDecision(underwear[difficulty]["lower"], rollLower)["lower"]})
     
     if (consoleOutput) {printResult("Underwear", result)}
     return result
 }
 
-function rollOutfit() {
-    rollTop = r10()
-    result = [{rollType: "Top", roll: rollTop, decision: findDecision(outfit[difficulty]["top"], rollTop)["top"]}]
+function rollOutfit(difficulty=default_difficulty) {
+    var rollTop = r10()
+    var result = [{rollType: "Top", roll: rollTop, decision: findDecision(outfit[difficulty]["top"], rollTop)["top"]}]
 
-    rollBottom = r10()
+    var rollBottom = r10()
     result.push({rollType: "Bottom", roll: rollBottom, decision: findDecision(outfit[difficulty]["bottom"], rollBottom)["bottom"]})
 
-    rollLimbs = r10()
+    var rollLimbs = r10()
     result.push({rollType: "Limbs", roll: rollLimbs, decision: findDecision(outfit[difficulty]["limbs"], rollLimbs)["limbs"]})
 
     if (consoleOutput) {printResult("Outfit", result)}
     return result
 }
 
-function rollBDSM() {
-    rollCount = r10()
-    result = [{rollType: "Count", roll: rollCount, decision: findDecision(bdsm[difficulty]["count"], rollCount)["count"]}]
+function rollBDSM(difficulty=default_difficulty) {
+    var rollCount = r10()
+    var result = [{rollType: "Count", roll: rollCount, decision: findDecision(bdsm[difficulty]["count"], rollCount)["count"]}]
 
     if (difficulty == "hard" || difficulty == "extreme") {
         result.push({rollType: "Gear", roll: "Automatic", decision: "Collar"})
     }
 
-    selected = []
-    i = 0
+    var selected = []
+    var i = 0
     while (i < result[0].decision) {
-        rollGear = r10()
-        gear = findDecision(bdsm[difficulty]["gear"], rollGear)["gear"]
+        var rollGear = r10()
+        var gear = findDecision(bdsm[difficulty]["gear"], rollGear)["gear"]
          if (selected.includes(gear)) {
             continue
         } else {
@@ -73,11 +73,11 @@ function rollBDSM() {
     return result
 }
 
-function rollWrists() {
-    rollUse = r10()
-    result = [{rollType: "Use", roll: rollUse, decision: findDecision(wrists[difficulty]["use"], rollUse)["use"]}]
+function rollWrists(difficulty=default_difficulty) {
+    var rollUse = r10()
+    var result = [{rollType: "Use", roll: rollUse, decision: findDecision(wrists[difficulty]["use"], rollUse)["use"]}]
     if (result[0].decision) {
-        rollBinding = r10()
+        var rollBinding = r10()
         result.push({rollType: "Binding", roll: rollBinding, decision: findDecision(wrists[difficulty]["binding"], rollBinding)["binding"]})
     }
 
@@ -85,11 +85,11 @@ function rollWrists() {
     return result
 }
 
-function rollAnkles() {
-    rollUse = r10()
-    result = [{rollType: "Use", roll: rollUse, decision: findDecision(ankles[difficulty]["use"], rollUse)["use"]}]
+function rollAnkles(difficulty=default_difficulty) {
+    var rollUse = r10()
+    var result = [{rollType: "Use", roll: rollUse, decision: findDecision(ankles[difficulty]["use"], rollUse)["use"]}]
     if (result[0].decision) {
-        rollBinding = r10()
+        var rollBinding = r10()
         result.push({rollType: "Binding", roll: rollBinding, decision: findDecision(ankles[difficulty]["binding"], rollBinding)["binding"]})
     }
 
@@ -241,77 +241,77 @@ export const underwear = {
 export const outfit = {
     easy: {
         top: [
-            {min: 1, max: 4, top: "None"}, // 40%
-            {min: 5, max: 9, top: "T-shirt"}, // 50%
-            {min: 10, max: 10, top: "Bodysuit"} // 10%
+            { min: 1, max: 4, top: "None"}, // 40%
+            { min: 5, max: 9, top: "T-shirt"}, // 50%
+            { min: 10, max: 10, top: "Bodysuit"} // 10%
         ],
         bottom: [
-            {min: 1, max: 4, bottom: "None"}, // 40%
-            {min: 5, max: 7, bottom: "Shorts"}, // 30%
-            {min: 8, max: 9, bottom: "Skirt"}, // 20%
-            {min: 10, max: 10, bottom: "Athletic Tight Shorts"}, // 10%
+            { min: 1, max: 4, bottom: "None"}, // 40%
+            { min: 5, max: 7, bottom: "Shorts"}, // 30%
+            { min: 8, max: 9, bottom: "Skirt"}, // 20%
+            { min: 10, max: 10, bottom: "Athletic Tight Shorts"}, // 10%
         ],
         limbs: [
-            {min: 1, max: 4, limbs: "None"}, // 40%
-            {min: 5, max: 6, limbs: "Thigh High Socks"}, // 20%
-            {min: 7, max: 8, limbs: "Arm Gloves"}, // 20%
-            {min: 9, max: 10, limbs: "Thigh High Socks + Arm Gloves"}, // 20%
+            { min: 1, max: 4, limbs: "None"}, // 40%
+            { min: 5, max: 6, limbs: "Thigh High Socks"}, // 20%
+            { min: 7, max: 8, limbs: "Arm Gloves"}, // 20%
+            { min: 9, max: 10, limbs: "Thigh High Socks + Arm Gloves"}, // 20%
         ]
     },
     medium: {
         top: [
-            {min: 1, max: 3, top: "None"}, // 30%
-            {min: 4, max: 8, top: "T-shirt"}, // 50%
-            {min: 9, max: 10, top: "Bodysuit"} // 20%
+            { min: 1, max: 3, top: "None"}, // 30%
+            { min: 4, max: 8, top: "T-shirt"}, // 50%
+            { min: 9, max: 10, top: "Bodysuit"} // 20%
         ],
         bottom: [
-            {min: 1, max: 3, bottom: "None"}, // 30%
-            {min: 4, max: 6, bottom: "Shorts"}, // 30%
-            {min: 7, max: 9, bottom: "Skirt"}, // 30%
-            {min: 10, max: 10, bottom: "Athletic Tight Shorts"}, // 10%
+            { min: 1, max: 3, bottom: "None"}, // 30%
+            { min: 4, max: 6, bottom: "Shorts"}, // 30%
+            { min: 7, max: 9, bottom: "Skirt"}, // 30%
+            { min: 10, max: 10, bottom: "Athletic Tight Shorts"}, // 10%
         ],
         limbs: [
-            {min: 1, max: 3, limbs: "None"}, // 30%
-            {min: 4, max: 5, limbs: "Thigh High Socks"}, // 20%
-            {min: 6, max: 7, limbs: "Arm Gloves"}, // 20%
-            {min: 8, max: 10, limbs: "Thigh High Socks + Arm Gloves"}, // 30%
+            { min: 1, max: 3, limbs: "None"}, // 30%
+            { min: 4, max: 5, limbs: "Thigh High Socks"}, // 20%
+            { min: 6, max: 7, limbs: "Arm Gloves"}, // 20%
+            { min: 8, max: 10, limbs: "Thigh High Socks + Arm Gloves"}, // 30%
         ]
     },
     hard: {
         top: [
-            {min: 1, max: 2, top: "None"}, // 20%
-            {min: 3, max: 7, top: "T-shirt"}, // 50%
-            {min: 8, max: 10, top: "Bodysuit"} // 30%
+            { min: 1, max: 2, top: "None"}, // 20%
+            { min: 3, max: 7, top: "T-shirt"}, // 50%
+            { min: 8, max: 10, top: "Bodysuit"} // 30%
         ],
         bottom: [
-            {min: 1, max: 2, bottom: "None"}, // 20%
-            {min: 3, max: 5, bottom: "Shorts"}, // 30%
-            {min: 6, max: 8, bottom: "Skirt"}, // 30%
-            {min: 9, max: 10, bottom: "Athletic Tight Shorts"}, // 20%
+            { min: 1, max: 2, bottom: "None"}, // 20%
+            { min: 3, max: 5, bottom: "Shorts"}, // 30%
+            { min: 6, max: 8, bottom: "Skirt"}, // 30%
+            { min: 9, max: 10, bottom: "Athletic Tight Shorts"}, // 20%
         ],
         limbs: [
-            {min: 1, max: 2, limbs: "None"}, // 20%
-            {min: 3, max: 4, limbs: "Thigh High Socks"}, // 20%
-            {min: 5, max: 6, limbs: "Arm Gloves"}, // 20%
-            {min: 7, max: 10, limbs: "Thigh High Socks + Arm Gloves"}, // 40%
+            { min: 1, max: 2, limbs: "None"}, // 20%
+            { min: 3, max: 4, limbs: "Thigh High Socks"}, // 20%
+            { min: 5, max: 6, limbs: "Arm Gloves"}, // 20%
+            { min: 7, max: 10, limbs: "Thigh High Socks + Arm Gloves"}, // 40%
         ]
     },
     extreme: {
         top: [
-            {min: 1, max: 1, top: "None"}, // 10%
-            {min: 2, max: 6, top: "T-shirt"}, // 50%
-            {min: 7, max: 10, top: "Bodysuit"} // 40%
+            { min: 1, max: 1, top: "None"}, // 10%
+            { min: 2, max: 6, top: "T-shirt"}, // 50%
+            { min: 7, max: 10, top: "Bodysuit"} // 40%
         ],
         bottom: [
-            {min: 1, max: 1, bottom: "None"}, // 10%
-            {min: 2, max: 4, bottom: "Shorts"}, // 30%
-            {min: 5, max: 7, bottom: "Skirt"}, // 30%
-            {min: 8, max: 10, bottom: "Athletic Tight Shorts"}, // 30%
+            { min: 1, max: 1, bottom: "None"}, // 10%
+            { min: 2, max: 4, bottom: "Shorts"}, // 30%
+            { min: 5, max: 7, bottom: "Skirt"}, // 30%
+            { min: 8, max: 10, bottom: "Athletic Tight Shorts"}, // 30%
         ],
         limbs: [
-            {min: 1, max: 2, limbs: "Thigh High Socks"}, // 20%
-            {min: 3, max: 4, limbs: "Arm Gloves"}, // 20%
-            {min: 5, max: 10, limbs: "Thigh High Socks + Arm Gloves"}, // 60%
+            { min: 1, max: 2, limbs: "Thigh High Socks"}, // 20%
+            { min: 3, max: 4, limbs: "Arm Gloves"}, // 20%
+            { min: 5, max: 10, limbs: "Thigh High Socks + Arm Gloves"}, // 60%
         ]
     }
 }
@@ -319,56 +319,56 @@ export const outfit = {
 export const bdsm = {
     easy: {
         count: [
-            {min: 1, max: 5, count: 0}, // 50%
-            {min: 6, max: 8, count: 1}, // 30%
-            {min: 9, max: 10, count: 2} // 20%
+            { min: 1, max: 5, count: 0}, // 50%
+            { min: 6, max: 8, count: 1}, // 30%
+            { min: 9, max: 10, count: 2} // 20%
         ],
         gear: [
-            {min: 1, max: 5, gear: "Collar"}, // 50%
-            {min: 6, max: 8, gear: "Blindfold"}, // 30%
-            {min: 9, max: 10, gear: "Earplugs"}, // 20%
+            { min: 1, max: 5, gear: "Collar"}, // 50%
+            { min: 6, max: 8, gear: "Blindfold"}, // 30%
+            { min: 9, max: 10, gear: "Earplugs"}, // 20%
         ]
     },
     medium: {
         count: [
-            {min: 1, max: 2, count: 0}, // 20%
-            {min: 3, max: 5, count: 1}, // 30%
-            {min: 6, max: 8, count: 2}, // 30%
-            {min: 9, max: 10, count: 3} // 20%
+            { min: 1, max: 2, count: 0}, // 20%
+            { min: 3, max: 5, count: 1}, // 30%
+            { min: 6, max: 8, count: 2}, // 30%
+            { min: 9, max: 10, count: 3} // 20%
         ],
         gear: [
-            {min: 1, max: 5, gear: "Collar"}, // 50%
-            {min: 6, max: 8, gear: "Blindfold"}, // 30%
-            {min: 9, max: 10, gear: "Earplugs"}, // 20%
+            { min: 1, max: 5, gear: "Collar"}, // 50%
+            { min: 6, max: 8, gear: "Blindfold"}, // 30%
+            { min: 9, max: 10, gear: "Earplugs"}, // 20%
         ]
     },
     hard: {
         count: [
-            {min: 1, max: 2, count: 0}, // 20%
-            {min: 3, max: 5, count: 1}, // 30%
-            {min: 6, max: 8, count: 2}, // 30%
-            {min: 9, max: 10, count: 3} // 20%
+            { min: 1, max: 2, count: 0}, // 20%
+            { min: 3, max: 5, count: 1}, // 30%
+            { min: 6, max: 8, count: 2}, // 30%
+            { min: 9, max: 10, count: 3} // 20%
         ],
         gear: [
             // Collar is automatic on hard
-            {min: 1, max: 4, gear: "Blindfold"}, // 40%
-            {min: 5, max: 8, gear: "Earplugs"}, // 40%
-            {min: 9, max: 10, gear: "Mouth Gag"}, // 20%
+            { min: 1, max: 4, gear: "Blindfold"}, // 40%
+            { min: 5, max: 8, gear: "Earplugs"}, // 40%
+            { min: 9, max: 10, gear: "Mouth Gag"}, // 20%
         ]
     },
     extreme: {
         count: [
-            {min: 1, max: 2, count: 1}, // 20%
-            {min: 3, max: 6, count: 2}, // 40%
-            {min: 7, max: 9, count: 3}, // 30%
-            {min: 10, max: 10, count: 4} // 10%
+            { min: 1, max: 2, count: 1}, // 20%
+            { min: 3, max: 6, count: 2}, // 40%
+            { min: 7, max: 9, count: 3}, // 30%
+            { min: 10, max: 10, count: 4} // 10%
         ],
         gear: [
             // Collar is automatic on extreme
-            {min: 1, max: 3, gear: "Blindfold"}, // 30%
-            {min: 4, max: 6, gear: "Earplugs"}, // 30%
-            {min: 7, max: 8, gear: "Mouth Gag"}, // 20%
-            {min: 9, max: 10, gear: "Chastity Cage"}, // 20%
+            { min: 1, max: 3, gear: "Blindfold"}, // 30%
+            { min: 4, max: 6, gear: "Earplugs"}, // 30%
+            { min: 7, max: 8, gear: "Mouth Gag"}, // 20%
+            { min: 9, max: 10, gear: "Chastity Cage"}, // 20%
         ]
     },
 }
@@ -462,7 +462,7 @@ export const ankles = {
     },
 }
 
-export default function CategoryDists(category) {
+export function categoryDists(category) {
     switch (category) {
         case "Buttplug": return buttplug
         case "Underwear": return underwear
@@ -472,3 +472,14 @@ export default function CategoryDists(category) {
         case "Ankles": return ankles    
     }
 }
+
+export function rollCategory(category, difficulty=default_difficulty) {
+    switch (category) {
+        case "Buttplug": return rollButtplug(difficulty)
+        case "Underwear": return rollUnderwear(difficulty)
+        case "Outfit": return rollOutfit(difficulty)
+        case "BDSM": return rollBDSM(difficulty)      
+        case "Wrists": return rollWrists(difficulty)
+        case "Ankles": return rollAnkles(difficulty)    
+    }
+}   
