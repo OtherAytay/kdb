b7344ce6046ab7c0483a861d0acc78c9cfa69b65
Other Aytay <OtherAytay@gmail.com>
2023-11-16 12:15:48 -0500

    Add file download utility

--- src/app/utilities.js
+++ src/app/utilities.js
@@ -1,9 +1,9 @@
 export const categoryDisplayName = {
-  "dildo": "Dildo",
-  "anal": "Anal Toy",
-  "bdsm": "BDSM Gear",
-  "clothing": "Clothing",
-  "cosmetic": "Cosmetic",
+    "dildo": "Dildo",
+    "anal": "Anal Toy",
+    "bdsm": "BDSM Gear",
+    "clothing": "Clothing",
+    "cosmetic": "Cosmetic",
 }
 
 export function displayDate(date) {
@@ -16,4 +16,19 @@ export function toDBName(displayName) {
 
 export function toDisplayName(dbName) {
     return dbName.trim().replaceAll("_", " ").split(" ").map((word) => word.slice(0, 1).toUpperCase() + word.slice(1)).join(' ')
+}
+
+export function saveAs(content, fileName) {
+    const a = document.createElement("a");
+    const isBlob = content.toString().indexOf("Blob") > -1;
+    let url = content;
+    if (isBlob) {
+        url = window.URL.createObjectURL(content);
+    }
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    if (isBlob) {
+        window.URL.revokeObjectURL(url);
+    }
 }
