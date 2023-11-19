ef06d2812a169d78ade2a3583154a4034058c0b8
Other Aytay <OtherAytay@gmail.com>
2023-11-18 19:55:13 -0500

    RatingBadge now uses forwardRef

--- src/app/components.js
+++ src/app/components.js
@@ -1,6 +1,7 @@
 import { useLiveQuery } from "dexie-react-hooks"
 import { db } from './db'
 import { Badge } from 'react-bootstrap'
+import { forwardRef } from "react"
 
 export function BrandLink({ id }) {
   const brand = useLiveQuery(() => db.brand.get(id)) 
@@ -8,9 +9,9 @@ export function BrandLink({ id }) {
   return (<a className="text-decoration-none text-primary-emphasis" target="blank_" rel="noopener noreferrer" href={brand.url}>{brand.name}</a>)
 }
 
-export function RatingBadge({rating}) {
-    return (<Badge as="a" bg="warning" className="text-decoration-none">{rating}<i className="bi bi-star-fill" /></Badge>)
-}
+export const RatingBadge = forwardRef(function RatingBadge(props, ref) {
+  return (<Badge {...props} ref={ref} as="a" bg="warning" className={"text-decoration-none " + props.className}>{props.rating}<i className="bi bi-star-fill" /></Badge>)
+}) 
 
 export function ImagePreviewModal({}) {
   return
